afe6a617be1e33d845e892f638ef63c7ce1e53a3
heaven7 <532278976@qq.com>
2021-11-25 10:11:37 +0800

    opt scheduler

--- pros/java-examples/src/main/java/com/bdfint/fgsb/terminal/Scheduler.java
+++ pros/java-examples/src/main/java/com/bdfint/fgsb/terminal/Scheduler.java
@@ -3,4 +3,11 @@ package com.bdfint.fgsb.terminal;
 public interface Scheduler {
 
     void schedule(Runnable task);
+
+    Scheduler DEFAULT = new Scheduler() {
+        @Override
+        public void schedule(Runnable task) {
+            new Thread(task).start();
+        }
+    };
 }
