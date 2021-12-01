2108c32ca6091156587c69037fcf3a7fed95573c
heaven7 <532278976@qq.com>
2021-12-01 19:03:34 +0800

    fix one-server to multi client.

--- pros/java-examples/src/main/java/com/bdfint/fgsb/terminal/ServerStreamManager.java
+++ pros/java-examples/src/main/java/com/bdfint/fgsb/terminal/ServerStreamManager.java
@@ -7,31 +7,32 @@ import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 
 public final class ServerStreamManager {
 
-    private static final String TAG = "ServerStreamManager";
-    private static final Logger logger =
+   // private static final String TAG = "ServerStreamManager";
+    private static final Logger sLogger =
             Logger.getLogger(ServerStreamManager.class.getName());
 
     private final ReqProcessor mProcessor;
-    private final LinkedList<ResMessage> mStack = new LinkedList<>();
     private final Scheduler mScheduler;
-
-    private Server mServer;
     private ExceptionProcessor mExceptionProcessor;
-    private final AtomicBoolean mCanceled = new AtomicBoolean(false);
+
+    private final List<UserSpace> mUsers = new ArrayList<>();
+    private Server mServer;
 
     public ServerStreamManager(Scheduler scheduler, ReqProcessor mProcessor) {
         this.mScheduler = scheduler;
         this.mProcessor = mProcessor;
     }
     /**
-     * start server with target the port
+     * start server with target the port. must called in single thread.
      * @param port the port to listen
      * @throws IOException if already started or shut down
      * IOException if unable to bind
@@ -40,68 +41,143 @@ public final class ServerStreamManager {
     public void start(int port) throws IOException, InterruptedException {
         final Server server = ServerBuilder
                 .forPort(port)
-                .addService(mSvc)
+                .addService(new IOStreamingImpl(this))
                 .build()
                 .start();
         mServer = server;
 
-        logger.info("Listening on " + server.getPort());
+        sLogger.info("Listening on " + server.getPort());
 
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
             public void run() {
                 // Use stderr here since the logger may have been reset by its JVM shutdown hook.
                 System.err.println("Shutting down");
-                end();
+                try {
+                    end();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
         server.awaitTermination();
-        logger.info("Server end. awaitTermination ");
+        sLogger.info("Server end. awaitTermination ");
     }
-
+    public void end() throws InterruptedException{
+        ArrayList<UserSpace> userSpaces = new ArrayList<>(mUsers);
+        synchronized (mUsers){
+            mUsers.clear();
+        }
+        for (UserSpace us: userSpaces){
+            us.end();
+        }
+        if(mServer != null){
+            mServer.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+            mServer = null;
+            sLogger.info("Server end. awaitTermination ");
+        }
+    }
     public void addMessage(ResMessage msg){
-        synchronized (mStack){
-            mStack.addLast(msg);
-        }
-        synchronized (this){
-            notify();
-        }
-    }
-
-    public void end(){
-        if(mCanceled.compareAndSet(false, true)){
-            synchronized (this){
-                notify();
+        mScheduler.schedule(new Runnable() {
+            @Override
+            public void run() {
+                sendMessage(msg);
             }
-            if(mServer != null){
-                try {
-                    mServer.shutdown().awaitTermination(30, TimeUnit.SECONDS);
-                    mServer = null;
-                } catch (InterruptedException e) {
-                    e.printStackTrace(System.err);
-                }
-            }
+        });
+    }
+    /**
+     * send message directly. must called in sub-thread.
+     * @param msg the message.
+     */
+    public void sendMessage(ResMessage msg){
+        for (UserSpace us: new ArrayList<>(mUsers)){
+            us.sendMessage(msg);
         }
-    }
-
-    public boolean isEnd(){
-        return mCanceled.get();
-    }
-    public void reset(){
-        mCanceled.set(false);
     }
     public void setExceptionProcessor(ExceptionProcessor mExceptionProcessor) {
         this.mExceptionProcessor = mExceptionProcessor;
     }
 
-    private final IOStreamingGrpc.IOStreamingImplBase mSvc = new IOStreamingGrpc.IOStreamingImplBase() {
+    private UserSpace newUserSpace(){
+        UserSpace userSpace = new UserSpace();
+        userSpace.setUp(mProcessor, mScheduler, mExceptionProcessor);
+        userSpace.mParent = this;
+        synchronized (mUsers){
+            mUsers.add(userSpace);
+        }
+        return userSpace;
+    }
+    public static class UserSpace{
+        private final LinkedList<ResMessage> mStack = new LinkedList<>();
+        private final AtomicBoolean mCanceled = new AtomicBoolean(false);
+        private ReqProcessor mProcessor;
+        private Scheduler mScheduler;
+        private ExceptionProcessor mExceptionProcessor;
+
+        private ServerStreamManager mParent;
+        private ServerCallStreamObserver<ResMessage> mObserver;
+
+        /*public*/ void setUp(ReqProcessor mProcessor, Scheduler mScheduler, ExceptionProcessor mExceptionProcessor) {
+            this.mProcessor = mProcessor;
+            this.mScheduler = mScheduler;
+            this.mExceptionProcessor = mExceptionProcessor;
+        }
+
+        public void end(){
+            if(mCanceled.compareAndSet(false, true)){
+                //mObserver.onCompleted();
+
+                synchronized (this){
+                    notify();
+                }
+                synchronized (mParent.mUsers){
+                    mParent.mUsers.remove(this);
+                }
+            }
+        }
+        public void addMessage(ResMessage msg){
+            mScheduler.schedule(new Runnable() {
+                @Override
+                public void run() {
+                    mObserver.onNext(msg);
+                }
+            });
+        }
+        /**
+         * send message right now. this must called in sub thread.
+         * @param msg the message
+         */
+        public void sendMessage(ResMessage msg){
+            mObserver.onNext(msg);
+        }
+        public boolean isEnd(){
+            return mCanceled.get();
+        }
+        public void reset(){
+            mCanceled.set(false);
+        }
+        public ServerStreamManager getServerStreamManager(){
+            return mParent;
+        }
+    }
+
+    private static class IOStreamingImpl extends IOStreamingGrpc.IOStreamingImplBase {
+
+        private final ServerStreamManager manager;
+
+        private IOStreamingImpl(ServerStreamManager manager) {
+            this.manager = manager;
+        }
         @Override
         public StreamObserver<ReqMessage> reqStreaming(StreamObserver<ResMessage> responseObserver) {
-            logger.info("reqStreaming");
+            sLogger.info("reqStreaming");
+            final UserSpace userSpace = manager.newUserSpace();
+
             ServerCallStreamObserver<ResMessage> resObserver = (ServerCallStreamObserver<ResMessage>) responseObserver;
-            OnReadyHandler mReadHandler = new OnReadyHandler(resObserver);
+            OnReadyHandler mReadHandler = new OnReadyHandler(userSpace, resObserver);
             resObserver.disableAutoRequest();
             resObserver.setOnReadyHandler(mReadHandler);
+            userSpace.mObserver = resObserver;
 //            resObserver.setOnCancelHandler(new Runnable() {
 //                @Override
 //                public void run() {
@@ -114,69 +190,46 @@ public final class ServerStreamManager {
 //                    System.out.println("server --- received close.");
 //                }
 //            });
-            return new ReqObserver(resObserver, mReadHandler);
+            return new ReqObserver(userSpace, resObserver, mReadHandler);
         }
     };
-    private class OnReadyHandler implements Runnable {
+    private static class OnReadyHandler implements Runnable {
 
+        final UserSpace userSpace;
         ServerCallStreamObserver<ResMessage> mObserver;
         boolean wasReady = false;
 
-        public OnReadyHandler(ServerCallStreamObserver<ResMessage> mObserver) {
+        public OnReadyHandler(UserSpace userSpace, ServerCallStreamObserver<ResMessage> mObserver) {
+            this.userSpace = userSpace;
             this.mObserver = mObserver;
         }
         @Override
         public void run() {
             if (mObserver.isReady() && !wasReady) {
                 wasReady = true;
-                logger.info("READY");
+                sLogger.info("READY");
 
                 mObserver.request(1);
             }
-            if(mScheduler == null){
-                logger.info("no scheduler: push message to client is disabled.");
-                return;
-            }
-            mScheduler.schedule(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        while (!mCanceled.get()){
-                            ResMessage req;
-                            synchronized (mStack){
-                                req = mStack.pollFirst();
-                            }
-                            if(req != null){
-                                mObserver.onNext(req);
-                            }else {
-                                System.out.println(TAG + " >>> no msg. wait for new msg.");
-                                synchronized (ServerStreamManager.this){
-                                    ServerStreamManager.this.wait();
-                                }
-                            }
-                        }
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                }
-            });
         }
     }
 
-    private class ReqObserver implements StreamObserver<ReqMessage>{
+    private static class ReqObserver implements StreamObserver<ReqMessage>{
 
+        final UserSpace userSpace;
         final ServerCallStreamObserver<ResMessage> mObserver;
         final OnReadyHandler mReadyHandler;
 
-        public ReqObserver(ServerCallStreamObserver<ResMessage> mObserver, OnReadyHandler mReadHandler) {
+        public ReqObserver(UserSpace userSpace, ServerCallStreamObserver<ResMessage> mObserver, OnReadyHandler mReadyHandler) {
+            this.userSpace = userSpace;
             this.mObserver = mObserver;
-            this.mReadyHandler = mReadHandler;
+            this.mReadyHandler = mReadyHandler;
         }
         @Override
         public void onNext(ReqMessage req) {
-            logger.info("server >>> onNext");
+            sLogger.info("server >>> onNext");
             try {
-                ResMessage res = mProcessor.process(ServerStreamManager.this, req);
+                ResMessage res = userSpace.mProcessor.process(userSpace, req);
                 mObserver.onNext(res);
                 if (mObserver.isReady()) {
                     mObserver.request(1);
@@ -184,8 +237,8 @@ public final class ServerStreamManager {
                     mReadyHandler.wasReady = false;
                 }
             }catch (Throwable e){
-                if(mExceptionProcessor != null){
-                    mExceptionProcessor.process(e);
+                if(userSpace.mExceptionProcessor != null){
+                    userSpace.mExceptionProcessor.process(userSpace, e);
                 }else{
                     e.printStackTrace();
                 }
@@ -195,25 +248,25 @@ public final class ServerStreamManager {
         }
         @Override
         public void onError(Throwable e) {
-            logger.info("server >>> onError");
+            sLogger.info("server >>> onError");
             mObserver.onCompleted();
-            if(mExceptionProcessor != null){
-                mExceptionProcessor.process(e);
+            if(userSpace.mExceptionProcessor != null){
+                userSpace.mExceptionProcessor.process(userSpace, e);
             }else{
                 e.printStackTrace();
             }
         }
         @Override
         public void onCompleted() {
-            logger.info("server >>> onCompleted");
+            sLogger.info("server >>> onCompleted");
             mObserver.onCompleted();
         }
     }
 
     public interface ReqProcessor{
-        ResMessage process(ServerStreamManager ctx, ReqMessage req);
+        ResMessage process(UserSpace userSpace, ReqMessage req);
     }
     public interface ExceptionProcessor{
-        void process(Throwable e);
+        void process(UserSpace userSpace, Throwable e);
     }
 }
